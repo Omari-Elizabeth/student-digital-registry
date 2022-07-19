3119e67f003e1cef0d1e060ef237fdb5c55ade1a
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-07-19 23:31:57 +0300

    introduce function chart.js

---
+++ script.js
@@ -0,0 +1,36 @@
+function dashboard(evt, dashboardName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(dashboardName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+
+var xValues = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+var yValues = [75, 90, 82, 48, 60];
+var barColors = ["red", "green","blue","orange","brown"];
+
+new Chart("myChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Weekly Attendance Chart"
+    }
+  }
+});
