9df4f137949715525f2158f86b8629ae93610ac4
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-07-21 01:32:45 +0300

    feat: create date method

--- ft-student/js/student.js
+++ ft-student/js/student.js
@@ -1,4 +1,4 @@
 document.getElementsByClassName('contacts').style.border="10px solid red";
-document.getElementById("p2").style.color = "red";
+
 // const today = new Date ();
 // document.getElementById(date).innerHTML = today;
