6876be19948aaf28866278e4ce0fc0a980ccf209
mercylynn <mercy.kamene@student.moringaschool.com>
2022-07-20 22:23:09 +0300

    add home page

---
+++ index.js
@@ -0,0 +1,27 @@
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "mercy@gmail.com",
+        Password : "password",
+        To : 'vobikot891@teasya.com',
+        From : document.getElementById("email.value"),
+        Subject : "Message from inquiry",
+        Body : "Name"+document.getElementById("name").value
+              + "Email"+document.getElementById("email").value
+              + "Phone no."+document.getElementById("phone").value
+              + "Message"+document.getElementById("message").value
+    }).then(
+      message => alert("message send successfully")
+    );
+}
+
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == form) {
+        modal.style.display = "none";
+    }
+}
+
