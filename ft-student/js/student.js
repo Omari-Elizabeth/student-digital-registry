9d77b8a102e0aac4b83d400361dea4b6edb9d6e0
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-07-21 09:39:44 +0300

    fixed: linked js to html

--- ft-student/js/student.js
+++ ft-student/js/student.js
@@ -1,4 +1,18 @@
-document.getElementsByClassName('contacts').style.border="10px solid red";
+function get_time() {
+    let time = document.getElementById('date').innerHTML = Date();
 
-// const today = new Date ();
-// document.getElementById(date).innerHTML = today;
+    return alert(`Happy Codind! It's on: ${time}`);
+};
+function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude;
+    }
+    var x = document.getElementById("demo");
